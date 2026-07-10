19737dd367c03337c45bb0eb2337e5871ac676ca
Sayed Alwedaie <5216479+sydalwedaie@users.noreply.github.com>
2026-07-10 21:04:35 +0300

    Fix incorrect grow logic expanding to 3 times.

--- src/hashmap.js
+++ src/hashmap.js
@@ -4,6 +4,7 @@ import { hash, loadSampleHashmap } from "./helpers.js";
 export function HashMap(capacity = 16, loadFactor = 0.75) {
   // Helper functions
   function init(capacity) {
+    hashmap.splice(0);
     for (let i = 0; i < capacity; i++) {
       hashmap.push(LinkedList());
     }
@@ -20,13 +21,12 @@ export function HashMap(capacity = 16, loadFactor = 0.75) {
   function grow() {
     capacity = capacity * 2;
     const entriesArr = entries();
-    clear();
     init(capacity);
     entriesArr.forEach((entry) => set(entry[0], entry[1]));
   }
 
   // Init hashmap array
-  let hashmap = [];
+  const hashmap = [];
   init(capacity);
 
   // Aux methods
@@ -45,7 +45,7 @@ export function HashMap(capacity = 16, loadFactor = 0.75) {
       bucket.prepend({ [key]: value });
     }
 
-    if (entries().length > capacity * loadFactor) {
+    if (length() > capacity * loadFactor) {
       grow();
     }
   };
@@ -107,58 +107,3 @@ export function HashMap(capacity = 16, loadFactor = 0.75) {
     entries,
   };
 }
-
-const test = HashMap();
-// loadSampleHashmap(test);
-// console.dir(test.getHashmap(), { depth: null });
-
-// override
-// test.set("Carlos", "I am the old value.");
-// test.set("Carlos", "I am the new value.");
-
-// collision
-// test.set("Rama", "value for Rama");
-// test.set("Sita", "value for Sita");
-// console.dir(test.getHashmap(), { depth: null });
-
-// console.log(test.print());
-// console.dir(test.getHashmap(), { depth: null });
-
-// console.log("# Test grow");
-// test.set("moon", "silver");
-// console.log(test.print());
-
-// console.log("# Test keys");
-// console.log(test.keys());
-
-// console.log("# Test values");
-// console.log(test.values());
-
-// console.log("# Test entries");
-// console.log(test.entries());
-
-// console.log("# Test length");
-// console.log("should print 12: ", test.length());
-// test.length();
-
-// console.log("# Test get");
-// loadSampleHashmap(test);
-// console.log("should print purple: ", test.get("grape"));
-// console.log("should print null: ", test.get("hello"));
-
-// console.log("# Test has");
-// console.log("should print true: ", test.has("grape"));
-// console.log("should print false: ", test.has("hello"));
-
-// console.log("# Test remove");
-// loadSampleHashmap(test);
-// console.log(test.remove("apple"));
-// console.log("hashmap does not include the apple entry");
-// console.dir(test.getHashmap(), { depth: null });
-
-// console.log("# Test keys");
-// console.log(test.keys());
-
-// console.log("# Test clear");
-// test.clear();
-// test.printHashmap();
